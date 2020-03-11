97ee68e6319af35d1fc78d4e3edae96fd38fa93e
PhilipVigus <philipvigus@yahoo.co.uk>
2020-03-11 20:02:09 +0000

    Rename method to ten_minute_walk?
    The method was  was renamed from ten_minute_walk to ten_minute_walk?
    so that it meets the specification of the challenge.

--- lib/ten_minute_walk.rb
+++ lib/ten_minute_walk.rb
@@ -1,4 +1,4 @@
-def ten_minute_walk(walk)
+def ten_minute_walk?(walk)
   if walk.length == 10
     walk.count('e') == walk.count('w') && walk.count('n') == walk.count('s')
   else
--- spec/ten_minute_walk_spec.rb
+++ spec/ten_minute_walk_spec.rb
@@ -2,18 +2,18 @@ require 'ten_minute_walk'
 
 describe 'ten minute walk method' do
   it 'returns false if there are not 10 steps' do
-    expect(ten_minute_walk(['w', 'w'])).to eq(false)
+    expect(ten_minute_walk?(['w', 'w'])).to eq(false)
   end
 
   it 'returns true if ten steps and circular' do
-    expect(ten_minute_walk(['e','w','e','w','e','w','e','w','e','w'])).to eq(true)
+    expect(ten_minute_walk?(['e','w','e','w','e','w','e','w','e','w'])).to eq(true)
   end
 
   it 'returns false if ten steps and not circular' do
-    expect(ten_minute_walk(['e','w','n','w','e','w','e','w','e','w'])).to eq(false)
+    expect(ten_minute_walk?(['e','w','n','w','e','w','e','w','e','w'])).to eq(false)
   end
 
   it 'returns false if not 10 steps and circular' do
-    expect(ten_minute_walk(['e', 'w']))
+    expect(ten_minute_walk?(['e', 'w']))
   end
 end
